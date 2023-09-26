fc46b0ba4e14e41017a174ed90366281257e0f0d
Kirill <106981545+vesnanervah@users.noreply.github.com>
2023-09-26 19:50:10 +0300

    feat: add nav-bar routing (#5)

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,25 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PageAboutComponent } from './page-about/page-about.component';
+import { PageStructuraComponent } from './page-structura/page-structura.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '', 
+    redirectTo: 'about', 
+    pathMatch: 'full'
+  },
+  {
+    title: 'about',
+    path: 'about',
+    component: PageAboutComponent
+  },
+  {
+    title: 'structura',
+    path: 'structura',
+    component: PageStructuraComponent
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/nav-bar/nav-bar.component.ts
+++ src/app/nav-bar/nav-bar.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { faAngleLeft, faAngleRight } from '@fortawesome/free-solid-svg-icons';
+import { NavBarItem } from 'src/types/nav-bar-item';
+import { Router, NavigationEnd } from '@angular/router'
 
 @Component({
   selector: 'app-nav-bar',
@@ -9,7 +11,34 @@ import { faAngleLeft, faAngleRight } from '@fortawesome/free-solid-svg-icons';
 export class NavBarComponent {
   iconPrev = faAngleLeft;
   iconNext = faAngleRight;
-  navItems = [
-    'О министерстве', 'Структура', 'Подведомственные организации', 'Образовательные учереждения', 'Информационные системы'
+  navItems: NavBarItem[] = [
+    {
+      label: 'О министерстве',
+      rout: 'about',
+    },
+    {
+      label: 'Структура',
+      rout: 'structura',
+    },
+    {
+      label: 'Подведомственные организации',
+      rout: 'pv-organizations',
+    },
   ];
+
+  constructor(
+    private router: Router,
+    ) {
+      router.events.subscribe((ev) => {
+        if(ev instanceof NavigationEnd) this.handleUrlChange();
+      })
+  }
+
+  private handleUrlChange() {
+    //i hate query search but in angular references to html elements works like shit
+    document.querySelectorAll('.nav-bar__item').forEach((item) => item.classList.remove('selected'));
+    const selector = `${this.router.url.slice(1)}`;
+    console.log(selector);
+    document.getElementById(selector)?.classList.add('selected');
+  }
 }
