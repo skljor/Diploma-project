14dd3dcdb3c98076d4d7a6007c54254fa439b544
Kirill <106981545+vesnanervah@users.noreply.github.com>
2023-10-01 20:42:33 +0300

    Feat/nav slider move (#7)
    * feat: add after element gradient
    
    * feat: add slider move

--- src/app/nav-bar/nav-bar.component.ts
+++ src/app/nav-bar/nav-bar.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { faAngleLeft, faAngleRight } from '@fortawesome/free-solid-svg-icons';
 import { NavItemData } from 'src/types/nav-item-data';
 import { Router, NavigationEnd } from '@angular/router';
@@ -8,7 +8,11 @@ import { Router, NavigationEnd } from '@angular/router';
   templateUrl: './nav-bar.component.html',
   styleUrls: ['./nav-bar.component.scss']
 })
-export class NavBarComponent {
+export class NavBarComponent implements AfterViewInit {
+  @ViewChild('navListElem') navList: ElementRef<HTMLDivElement> | undefined;
+  @ViewChild('navListWrapper') navListWrapper: ElementRef<HTMLDivElement> | undefined;
+  @ViewChild('prevBtn') prevBtn: ElementRef<HTMLButtonElement> | undefined;
+  @ViewChild('nextBtn') nextBtn: ElementRef<HTMLButtonElement> | undefined;
   iconPrev = faAngleLeft;
   iconNext = faAngleRight;
   selectedItem: NavItemData | undefined;
@@ -25,7 +29,42 @@ export class NavBarComponent {
       label: 'Подведомственные организации',
       route: 'pv-organizations',
     },
+    {
+      label: 'Образовательные учереждения',
+      route: 'ed-establisments'
+    },
+    {
+      label: 'Информационные системы',
+      route: 'it-systems'
+    },
+    {
+      label: 'Вакансии',
+      route: 'jobs'
+    },
+    {
+      label: 'Кадровый резерв',
+      route: 'rezerv'
+    },
+    {
+      label: 'Открытые данные',
+      route: 'opendata'
+    },
+    {
+      label:'Планы информатизации',
+      route: '4563'
+    },
+    {
+      label: 'Обеспечение безопасности',
+      route: '1254'
+    },
+    {
+      label: 'Контакты и реквизиты',
+      route: 'contacs'
+    }
   ];
+  private sliderPos = 0;
+  private maxSliderPos = 0;
+  private sliderStep = 0;
 
   constructor(private router: Router) {
     this.router.events.subscribe((event) => {
@@ -35,4 +74,47 @@ export class NavBarComponent {
     });
   }
 
+  ngAfterViewInit(): void {
+    this.calculateStep();
+    this.handleSliderChanges();
+  }
+
+  private calculateStep(): void {
+    if(this.navList && this.navListWrapper) {
+      this.sliderStep = Math.ceil((this.navList?.nativeElement.scrollWidth - this.navListWrapper.nativeElement.offsetWidth) / this.navItems.length);
+      this.maxSliderPos = this.navItems.length;
+    }
+  }
+
+  handleNextClick() {
+    if (this.sliderPos < this.maxSliderPos) {
+      this.sliderPos += 1;
+      this.handleSliderChanges();
+    }
+  }
+
+  handlePrevClick() {
+    if (this.sliderPos > 0) {
+      this.sliderPos -= 1;
+      this.handleSliderChanges();
+    }
+  }
+
+  private handleSliderChanges() {
+    if (this.navList) {
+      this.navList.nativeElement.style.transform = `translateX(-${this.sliderPos * this.sliderStep}px)`;
+    }
+    if(this.sliderPos > 0 && this.prevBtn) {
+      this.prevBtn.nativeElement.classList.add('active-btn')
+    }
+    if(this.sliderPos < this.maxSliderPos && this.nextBtn) {
+      this.nextBtn.nativeElement.classList.add('active-btn')
+    }
+    if(this.sliderPos === 0 && this.prevBtn) {
+      this.prevBtn.nativeElement.classList.remove('active-btn')
+    }
+    if(this.sliderPos === this.maxSliderPos && this.nextBtn) {
+      this.nextBtn.nativeElement.classList.remove('active-btn')
+    }
+  }
 }
