850de44af5b8f2455b43b331a0f13111069209ce
Kirill <106981545+vesnanervah@users.noreply.github.com>
2023-10-02 10:58:47 +0300

    feat: add 404 page and route, fix minor bugs of nav-bar (#8)

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { PageAboutComponent } from './page-about/page-about.component';
 import { PageStructuraComponent } from './page-structura/page-structura.component';
+import { Page404Component } from './page404/page404.component';
 
 const routes: Routes = [
   {
@@ -19,6 +20,10 @@ const routes: Routes = [
     path: 'structura',
     component: PageStructuraComponent
   },
+  {
+    path: '**',
+    component: Page404Component
+  }
 ];
 
 @NgModule({
