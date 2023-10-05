268a0d4d1554ffb153dc6b66748e8de83857c396
Kirill <106981545+vesnanervah@users.noreply.github.com>
2023-10-05 22:02:31 +0300

    feat/structures endpoint and structures data (#13)

--- server/dist/index.js
+++ server/dist/index.js
@@ -4,8 +4,15 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
+const structures_1 = require("./data/structures");
 const app = (0, express_1.default)();
 const PORT = 5000;
+app.get('/', (req, res) => {
+    res.send('Api is working');
+});
+app.get('/structures', (req, res) => {
+    res.send(structures_1.structures);
+});
 app.listen(PORT, () => {
     console.log('The backend is online on port' + PORT);
 });
--- server/src/index.ts
+++ server/src/index.ts
@@ -1,7 +1,16 @@
 import express from 'express';
+import { structures } from './data/structures';
 
 const app = express();
 const PORT = 5000;
+
+app.get('/', (req, res) => {
+  res.send('Api is working');
+})
+
+app.get('/structures', (req, res) => {
+  res.send(structures);
+});
 
 app.listen(PORT, () => {
   console.log('The backend is online on port' + PORT);
